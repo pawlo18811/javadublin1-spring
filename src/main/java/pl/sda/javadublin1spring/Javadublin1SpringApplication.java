25f5de3eac8cad777ca59c0d4232359c5ccc0c26
Szymon Nowak <nowszy94@gmail.com>
2019-09-15 12:40:17 +0100

    Spring core (#1)
    * simple component and autowired
    
    * users application
    
    * qualifier and primary annotations and named beans
    
    * profiles
    
    * beans scopes
    
    * todo exercise part 1
    
    * todo exercise part 2

--- src/main/java/pl/sda/javadublin1spring/Javadublin1SpringApplication.java
+++ src/main/java/pl/sda/javadublin1spring/Javadublin1SpringApplication.java
@@ -1,13 +1,29 @@
 package pl.sda.javadublin1spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.sda.javadublin1spring.user.FileBasedUserRepository;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Javadublin1SpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Javadublin1SpringApplication.class, args);
-	}
+    @Autowired
+    private HelloBean helloBean;
 
+    @Autowired
+    private FileBasedUserRepository fileBasedUserRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javadublin1SpringApplication.class, args);
+        System.out.println("blabal");
+
+    }
+
+    @PostConstruct
+    public void run() {
+        helloBean.hello();
+    }
 }
---
+++ src/main/java/pl/sda/javadublin1spring/user/UserService.java
@@ -0,0 +1,23 @@
+package pl.sda.javadublin1spring.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
